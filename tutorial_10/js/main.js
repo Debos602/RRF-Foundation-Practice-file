c1c43cb3300d41069448cdeb9123e4c6f4f4f10a
Debos <debos.das.02@gmail.com>
2023-11-13 13:13:59 +0600

    commit 16 til

--- tutorial_10/js/main.js
+++ tutorial_10/js/main.js
@@ -2,7 +2,6 @@
 	"use strict";
 
 	jQuery(document).ready(function ($) {
-		
 		$(".client-testimonial-carousel").owlCarousel({
 			items: 3,
 			autoplay: false,
@@ -10,6 +9,36 @@
 			loop: true,
 			nav: false,
 		});
-		jQuery(window).load(function () {});
+		jQuery(window).load(function () {
+			$(".product-list").masonry();
+			$(".homepage-slides").owlCarousel({
+				items: 1,
+				loop: true,
+				autoplay: false,
+				dots: false,
+				nav: true,
+				navText: [
+					"<i class='fas fa-arrow-left'></i>",
+					"<i class='fas fa-arrow-right'></i>",
+				],
+			});
+			$(".product-promotion").owlCarousel({
+				items: 1,
+				loop: true,
+				autoplay: false,
+				dots: true,
+				nav: false,
+			});
+			$(".menu-trigger").on("click", function () {
+				console.log("text");
+				$(".off-canvas-menu, .of-canvas-overlay").addClass("active");
+				return false;
+			});
+			$(".menu-close, .of-canvas-overlay").on("click", function () {
+				console.log("text");
+				$(".off-canvas-menu, .of-canvas-overlay").removeClass("active");
+				return false;
+			});
+		});
 	});
 })(jQuery);
