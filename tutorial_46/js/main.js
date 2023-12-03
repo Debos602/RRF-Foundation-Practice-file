2725b1758221d4887aee04fbd05075651a4c8bb2
Debos <debos.das.02@gmail.com>
2023-12-04 02:00:08 +0600

    tutorial 46 no

---
+++ tutorial_46/js/main.js
@@ -0,0 +1,13 @@
+(function ($) {
+	"use strict";
+	jQuery(document).ready(function ($) {
+		$(".featured-tailor-carousel").owlCarousel({
+			items: 1,
+			loop: true,
+			autoplay: false,
+			nav: false,
+			dots: true,
+		});
+	});
+	jQuery(window).load(function () {});
+})(jQuery);
