e85d5602957d58a573e37c087172a41bac13c1e1
Debos <debos.das.02@gmail.com>
2024-01-02 19:53:40 +0600

    Flip box

---
+++ tutorial_84/js/main.js
@@ -0,0 +1,14 @@
+(function ($) {
+	"use strict";
+
+	$(document).ready(function () {
+		$(".hover").hover(
+			function () {
+				$(this).addClass("flip");
+			},
+			function () {
+				$(this).removeClass("flip");
+			}
+		);
+	});
+})(jQuery);
