c65f31ce013f66025b680684141e9d3c3bd1df99
Debos <debos.das.02@gmail.com>
2024-01-19 01:50:21 +0600

    youtube background video

--- tutorial_108/js/main.js
+++ tutorial_108/js/main.js
@@ -1,16 +1,8 @@
 (function ($) {
 	"use strict";
 	$(document).ready(function () {
-		$(".homepage-slides").owlCarousel({
-			items: 1,
-			margin: 10,
-			loop: true,
-			autoplayTimeout: 1000,
-			nav: true,
-			navText: [
-				"<i class='fa-solid fa-arrow-left-long'></i>",
-				"<i class='fa-solid fa-arrow-right-long'></i>",
-			],
-		});
+		jQuery("[data-vbg]").youtube_background();
+
+		const videoBackgrounds = VIDEO_BACKGROUNDS;
 	});
 })(jQuery);
