c5dc7fe7639e31645f8d486eb8ac3cac89d6695d
Debos <debos.das.02@gmail.com>
2024-01-20 22:03:39 +0600

    positioning image without absolute relative for perfect responsive

---
+++ tutorial_113/js/main.js
@@ -0,0 +1,27 @@
+(function ($) {
+	"use strict";
+	$(document).ready(function () {
+		$(".portfolio-list").masonry({
+			horizontalOrder: true,
+		});
+
+		$(".portfolio-filter li").on("click", function () {
+			$(".portfolio-filter li").removeClass("active");
+			$(this).addClass("active");
+			var filterData = $(this).attr("data-filter");
+			$(".portfolio-list").isotope({
+				filter: filterData,
+				itemSelector: ".single-portfolio-item",
+				percentPosition: true,
+				masonry: {
+					// use element for option
+					columnWidth: ".single-portfolio-item",
+					horizontalOrder: true,
+				},
+			});
+		});
+
+		// Initialize Perfect Scrollbar
+		const ps = new PerfectScrollbar(".container");
+	});
+})(jQuery);
