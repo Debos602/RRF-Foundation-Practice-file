5000ea1ed304dbf02c2d6cffb4b345928a3813e7
Debos <debos.das.02@gmail.com>
2024-01-22 00:21:27 +0600

    Drop down and transform effect

---
+++ tutorial_119/js/main.js
@@ -0,0 +1,11 @@
+(function ($) {
+	"use strict";
+	$(document).ready(function () {
+		$(".portfolio-items").owlCarousel({
+			items: 5,
+			loop: true,
+			nav: true,
+			dots: true,
+		});
+	});
+})(jQuery);
