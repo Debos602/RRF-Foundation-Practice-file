5b0e1953a7c9bf665a9d863a081fea6e13376af4
Debos <debos.das.02@gmail.com>
2024-01-23 23:43:59 +0600

    Image and content tow side by image src proper use of margin padding

---
+++ tutorial_127/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+	$(document).ready(function () {
+		$(".portfolio-list").masonry();
+
+		$(".single-portfolio-item").hover(
+			function () {
+				$(this)
+					.find(".portfolio-hover h2")
+					.addClass("animate__animated animate__flipInX");
+			},
+			function () {
+				$(this)
+					.find(".portfolio-hover h2")
+					.removeClass("animate__animated animate__flipInX");
+			}
+		);
+		$(".menu-trigger").on("click", function () {
+			$(".offcanvs-menu").addClass("active");
+			$(".offcanvas-overlay").addClass("active");
+		});
+		$(".menu-close,.offcanvas-overlay").on("click", function () {
+			$(".offcanvs-menu").removeClass("active");
+			$(".offcanvas-overlay").removeClass("active");
+		});
+		$(".header-area").headroom();
+	});
+})(jQuery);
